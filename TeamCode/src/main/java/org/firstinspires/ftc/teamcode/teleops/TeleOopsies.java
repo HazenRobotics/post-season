7669561299bace1b633ab68dd808db916e77a96f
sirpoetvic <victor.wong88@hotmail.com>
2022-05-11 07:45:26 -0700

    Teleop works, with adjusted motor power values.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/TeleOopsies.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/TeleOopsies.java
@@ -27,8 +27,6 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode.teleops.placeholderName;
-
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -42,64 +40,64 @@ import com.qualcomm.robotcore.util.Range;
 @TeleOp(name="TeleOopsies - team Pancit", group="Linear Opmode")
 //@Disabled
 public class TeleOopsies extends LinearOpMode {
-    DcMotorEx frontLeft;
-    DcMotorEx backLeft;
-    DcMotorEx frontRight;
-    DcMotorEx backRight;
+	DcMotorEx frontLeft;
+	DcMotorEx backLeft;
+	DcMotorEx frontRight;
+	DcMotorEx backRight;
 
-    @Override
-    public void runOpMode() {
-        frontLeft = hardwareMap.get( DcMotorEx.class, "frontLeft" );
-        backLeft = hardwareMap.get( DcMotorEx.class, "backLeft" );
-        frontRight = hardwareMap.get( DcMotorEx.class, "frontRight" );
-        backRight = hardwareMap.get( DcMotorEx.class, "backRight" );
+	@Override
+	public void runOpMode() {
+		frontLeft = hardwareMap.get( DcMotorEx.class, "frontLeft" );
+		backLeft = hardwareMap.get( DcMotorEx.class, "backLeft" );
+		frontRight = hardwareMap.get( DcMotorEx.class, "frontRight" );
+		backRight = hardwareMap.get( DcMotorEx.class, "backRight" );
 
-        frontRight.setDirection(DcMotor.Direction.REVERSE);
-        backRight.setDirection(DcMotor.Direction.REVERSE);
+		frontRight.setDirection(DcMotor.Direction.REVERSE);
+		backRight.setDirection(DcMotor.Direction.REVERSE);
 
-        waitForStart();
+		waitForStart();
 
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-            move( gamepad1.left_stick_y, gamepad1.left_stick_x, -gamepad1.right_stick_x );
+		// run until the end of the match (driver presses STOP)
+		while (opModeIsActive()) {
+			move( gamepad1.left_stick_y, gamepad1.left_stick_x, -gamepad1.right_stick_x );
 
-        }
-    }
+		}
+	}
 
 
-    public void setPower( double frontLeftPower, double backLeftPower, double frontRightPower, double backRightPower ) {
-        frontLeft.setPower( frontLeftPower * 0.54 );
-        backLeft.setPower( backLeftPower * 0.54 );
-        frontRight.setPower( frontRightPower );
-        backRight.setPower( backRightPower * 0.54 );
-    }
+	public void setPower( double frontLeftPower, double backLeftPower, double frontRightPower, double backRightPower ) {
+		frontLeft.setPower( frontLeftPower * 0.54 );
+		backLeft.setPower( backLeftPower * 0.54 );
+		frontRight.setPower( frontRightPower );
+		backRight.setPower( backRightPower * 0.54 );
+	}
 
-    public void drive( double power ) {
-        setPower( power, power, power, power );
-    }
+	public void drive( double power ) {
+		setPower( power, power, power, power );
+	}
 
-    public void strafe( double power ) {
-        setPower( power, -power, -power, power );
-    }
+	public void strafe( double power ) {
+		setPower( power, -power, -power, power );
+	}
 
-    public void rotate( double power ) {
-        setPower( power, power, -power, -power);
-    }
+	public void rotate( double power ) {
+		setPower( power, power, -power, -power);
+	}
 
-    public void stopRobot( ) {
-        frontLeft.setPower( 0 );
-        backLeft.setPower( 0 );
-        frontRight.setPower( 0 );
-        backRight.setPower( 0 );
-    }
+	public void stopRobot( ) {
+		frontLeft.setPower( 0 );
+		backLeft.setPower( 0 );
+		frontRight.setPower( 0 );
+		backRight.setPower( 0 );
+	}
 
-    public void move( double drive, double strafe, double rotate ) {
-        double frontLeftPower = drive + rotate - strafe;
-        double backLeftPower = drive + rotate + strafe;
-        double frontRightPower = drive - rotate + strafe;
-        double backRightPower = drive - rotate - strafe;
-        setPower( frontLeftPower, backLeftPower, frontRightPower, backRightPower );
-    }
+	public void move( double drive, double strafe, double rotate ) {
+		double frontLeftPower = drive + rotate - strafe;
+		double backLeftPower = drive + rotate + strafe;
+		double frontRightPower = drive - rotate + strafe;
+		double backRightPower = drive - rotate - strafe;
+		setPower( frontLeftPower, backLeftPower, frontRightPower, backRightPower );
+	}
 
 
 
