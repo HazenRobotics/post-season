9de25d5935bce1ec3f39d3d2fc64a2dc3ae5a188
Cjh4444 <camdenharris@gmail.com>
2022-05-18 15:19:55 -0700

    Fixed Servo issues

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/AlexTeamTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/AlexTeamTeleOp.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "AlexTeamTeleOp", group = "TeleOp")
-public class AlexTeamTeleOP extends OpMode {
+class AlexTeamTeleOP extends OpMode {
 	DcMotor frontLeft, backRight, frontRight, backLeft;
 	Servo clearance, cleared, lift, claw;
 
